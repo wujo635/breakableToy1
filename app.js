3b3fafd211cd1e182acb7945289807b18399af6c
Johnny Wu <wujo@johnnys-mbp.home>
2019-04-06 22:44:16 -0400

    Setup landing page ejs

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 8080);
 
+app.get("/", (request, response) => {
+    response.render("index");
+});
+
+app.get("*", (request,response) => {
+    console.log("Unexpected GET");
+    response.render("index");
+});
+
 const server = app.listen(app.get('port'), () => {
     console.log("Server started");
 });
